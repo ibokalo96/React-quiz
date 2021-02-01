7f194f3817a4ae2be3c197416e8848740bff79bc
Igor <ibokalo96@gmail.com>
2021-02-01 15:36:20 +0200

    second question done

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -6,15 +6,16 @@ const ActiveQuiz = props => (
     <div className={classes.ActiveQuiz}>
         <p className={classes.Question}>
             <span>
-                <strong>2.</strong>&nbsp;
-                 Как дела? 
+                <strong>{props.answerNumber}.</strong>&nbsp;
+                 {props.question}
             </span>
 
-            <small>4 из 12</small>
+            <small>{props.answerNumber} из  {props.quizLength} </small>
         </p>
 
         <AnswerList 
         answers={props.answers}
+        onAnswerClick={props.onAnswerClick}
         />
     </div>
 )
